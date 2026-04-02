1d0d58b0f7fdf0575e9ec502ab3e5de896663926
lgcirilo <lgcirilo@gmail.com>
2026-04-02 16:33:53 -0700

    created main file
    created dockerfile
    
    created docker compose file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/lgcirilo/go-demo-api/internal/config"
+	"github.com/lgcirilo/go-demo-api/internal/db"
+	"github.com/lgcirilo/go-demo-api/internal/handler"
+	"github.com/lgcirilo/go-demo-api/internal/repository"
+	"github.com/lgcirilo/go-demo-api/internal/service"
+)
+
+func main() {
+	cfg := config.Load()
+
+	dbPool := db.NewDBPool(cfg.DBUrl)
+
+	userRepo := repository.NewUserRepository(dbPool)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	r := gin.Default()
+
+	r.POST("/users", userHandler.CreateUser)
+	r.GET("/users", userHandler.GetUsers)
+
+	r.Run(":" + cfg.Port)
+}
