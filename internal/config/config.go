5e1e22062c2e923b35f9ba81346335e041ffc543
lgcirilo <lgcirilo@gmail.com>
2026-04-02 15:26:54 -0700

    DB config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,3 +6,23 @@ import (
 
 	"github.com/joho/godotenv"
 )
+
+type Config struct {
+	DBUrl string
+	Port string
+}
+
+func Load() *Config {
+
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+		// log.Fatal("No .env file found")
+	}
+
+	return &Config{
+		DBUrl: os.Getenv("DATABASE_URL"),
+		Port: os.Getenv("PORT"),
+	}
+}
