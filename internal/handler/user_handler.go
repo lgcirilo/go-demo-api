6723f91effa1b001544a9a3563290f93cec76c56
lgcirilo <lgcirilo@gmail.com>
2026-04-02 16:24:03 -0700

    created empty main file

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,1 +1,45 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lgcirilo/go-demo-api/internal/models"
+	"github.com/lgcirilo/go-demo-api/internal/service"
+)
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func NewUserHandler(s *service.UserService) *UserHandler {
+	return &UserHandler{service: s}
+}
+
+func (h *UserHandler) CreateUser(c *gin.Context) {
+	var user models.User
+
+	// are the following two if statements different representations of the same thing?
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := h.service.CreateUser(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, user)
+}
+
+func (h *UserHandler) GetUsers(c *gin.Context) {
+	users, err := h.service.GetUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+}
