8f592953fb7518efcad3c239bc2113feb98d6c90
lgcirilo <lgcirilo@gmail.com>
2026-04-02 16:13:14 -0700

    code for user service

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"github.com/lgcirilo/go-demo-api/internal/models"
+	"github.com/lgcirilo/go-demo-api/internal/repository"
+)
+
+type UserService struct {
+	repo *repository.UserRepository
+}
+
+func NewUserService(r *repository.UserRepository) *UserService {
+	return &UserService{repo: r}
+}
+
+func (s *UserService) CreateUser(user *models.User) error {
+	return s.repo.Create(user)
+}
+
+func (s *UserService) GetUsers() ([]models.User, error) {
+	return s.repo.GetAll()
+}
